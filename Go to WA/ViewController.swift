30ef4a6b9842981c58c9085d1a5f4a926f5bdd86
Dmitry Meshechkov <d.meshechkov@yandex.ru>
2022-11-22 22:38:19 +0500

    Made desine and IBAction&IBOutlet

--- Go to WA/ViewController.swift
+++ Go to WA/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var inputNumberTF: UITextField!
+    @IBOutlet weak var WhatsAppLabel: UILabel!
+    @IBOutlet weak var TelegaLabel: UILabel!
+    
+    
+    @IBAction func whatsappTapped(_ sender: UIButton) {
     }
-
-
+    @IBAction func telegramTapped(_ sender: UIButton) {
+    }
+    @IBAction func clearTapped(_ sender: UIButton) {
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
 
