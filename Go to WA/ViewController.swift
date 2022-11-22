0cc6a96d988a7a645717d634dc9430253069b236
Dmitry Meshechkov <d.meshechkov@yandex.ru>
2022-11-23 01:30:14 +0500

    Clear the number from charasters

--- Go to WA/ViewController.swift
+++ Go to WA/ViewController.swift
@@ -9,9 +9,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var inputNumberTF: UITextField!
+    @IBOutlet private weak var inputNumberTF: UITextField!
     @IBOutlet weak var WhatsAppLabel: UILabel!
     @IBOutlet weak var TelegaLabel: UILabel!
+    
+    private let maxNumberCount = 11
+    private let regex = try! NSRegularExpression(pattern: "[\\+\\s-\\(\\)]", options: .caseInsensitive)
+    
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+      
+    inputNumberTF.delegate = self
+    inputNumberTF.keyboardType = .numberPad
+    
+    }
+        
+    
+    private func format(phoneNumber: String, shouldRemoveLastDigit: Bool) -> String {
+        
+        guard !(shouldRemoveLastDigit && phoneNumber.count <= 2) else { return "7" }
+        
+        let range = NSString(string: phoneNumber).range(of: phoneNumber)
+        var number = regex.stringByReplacingMatches(in: phoneNumber, options: [], range: range, withTemplate: "")
+        
+        if number.count > maxNumberCount {
+            let maxIndex = number.index(number.startIndex, offsetBy: maxNumberCount)
+                number = String(number[number.startIndex..<maxIndex])
+               }
+        
+        if shouldRemoveLastDigit {
+            let maxIndex = number.index(number.startIndex, offsetBy: number.count - 1)
+                number = String(number[number.startIndex..<maxIndex])
+                }
+        
+        let maxIndex = number.index(number.startIndex, offsetBy: number.count)
+        let regRange = number.startIndex..<maxIndex
+        
+//        if number.count < 7 {
+//            let pattern = "(\\d)(\\d{3})(\\d+)"
+//                number = number.replacingOccurrences(of: pattern, with: "$1$2$3", options: .regularExpression, range: regRange)
+//            } else {
+//                let pattern = "(\\d)(\\d{3})(\\d{3})(\\d{2})(\\d+)"
+//                    number = number.replacingOccurrences(of: pattern, with: "$1$2$3$4$5", options: .regularExpression, range: regRange)
+//                }
+        
+        return number
+    }
+    
     
     
     @IBAction func whatsappTapped(_ sender: UIButton) {
@@ -26,3 +72,10 @@ class ViewController: UIViewController {
     }
 }
 
+extension ViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let fullString = (textField.text ?? "") + string
+        textField.text = format(phoneNumber: fullString, shouldRemoveLastDigit: range.length == 1)
+        return false
+    }
+}
